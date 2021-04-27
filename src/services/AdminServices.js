2278fa6fdd3d38bd1353707b99a0c426b998c867
AnnaM135 <68158093+AnnaM135@users.noreply.github.com>
2021-04-27 17:13:06 +0400

    server

--- src/services/AdminServices.js
+++ src/services/AdminServices.js
@@ -4,8 +4,8 @@ import Axios from "axios"
 class AdminService {
     constructor() {
         this.api = Axios.create({
-             baseURL: "http://localhost:8888/api",
-           //baseURL: "http://46.4.249.19:8888/api",
+            // baseURL: "http://localhost:8888/api",
+           baseURL: "http://46.4.249.19:8888/api",
             headers: {
                 'Content-type': 'application/json'
             },
